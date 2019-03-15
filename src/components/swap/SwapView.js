e943e680a40e2a1b1ab9d4be528d051cf5035523
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2019-03-15 16:21:41 +0700

    remove not use chracter

--- src/components/swap/SwapView.js
+++ src/components/swap/SwapView.js
@@ -42,7 +42,7 @@ export default class SwapView extends Component {
             </div>
 
             <div className={"input-group__info"}>
-              1 {this.props.sourceToken.symbol} = {isLoadingRateShown ? <div className={"input-group__loading common__loading"}/> : formatAmount(this.props.tokenPairRate)} {this.props.destToken.symbol}
+              {this.props.sourceToken.symbol} = {isLoadingRateShown ? <div className={"input-group__loading common__loading"}/> : formatAmount(this.props.tokenPairRate)} {this.props.destToken.symbol}
             </div>
           </div>
         </div>
