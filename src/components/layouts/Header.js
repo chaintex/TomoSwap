7b2e9289ea4504c916d41c97d882d9a6b8366c2b
Le Xuan Manh <manhlx.300695@gmail.com>
2019-03-19 10:26:35 +0700

    Update more links to social, blog, etc (#41)

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -28,10 +28,10 @@ class Header extends Component {
           </div>
           <div className={"header__content"}>
             <a href="/" className={"header__content-item active"}>Swap</a>
-            <a href="/" className={"header__content-item"}>Why Us</a>
+            <a href="/" className={"header__content-item"}>About Us</a>
             <a href="/" className={"header__content-item"}>FAQ</a>
             <a href="https://goo.gl/forms/PPgKR2d6A5KtV7tH2" target="_blank" rel="noopener noreferrer" className={"header__content-item"}>Contact Us</a>
-            <a href="/" className={"header__content-item"}>Blog</a>
+            <a href="https://medium.com/@tomoswap" target="_blank" rel="noopener noreferrer" className={"header__content-item"}>Blog</a>
           </div>
         </div>
       </div>
