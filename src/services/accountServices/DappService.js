c223244d9fede5e0fbb0b1285ef2a4f120b86cd6
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2019-03-25 15:43:44 +0700

    support dapp browser

---
+++ src/services/accountServices/DappService.js
@@ -0,0 +1,11 @@
+import Web3 from "web3"
+
+export default class DappService {
+  constructor() {
+    this.web3 = new Web3(Web3.givenProvider || window.web3.currentProvider || window.web3.givenProvider)
+    //for older verions of web3
+    if (this.web3 && this.web3.net && !this.web3.eth.net){
+      this.web3.eth.net = this.web3.net
+    }
+  }
+} 
