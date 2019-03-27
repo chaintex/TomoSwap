a2aa16b86985e11f707d5b227678f24aa2861dd8
Mark Nguyen <ducthuan2807@gmail.com>
2019-03-27 18:36:16 +0700

    Support dapp browser tomo (#77)
    * support dapp browser
    
    * fix error load address
    
    * refactor code
    
    * refactor code
    
    * refactor code
    
    * remove unused code

--- src/services/accountServices/DappService.js
+++ src/services/accountServices/DappService.js
@@ -1,11 +1,37 @@
-import Web3 from "web3"
+import { getWeb3Instance } from "../web3Service";
 
 export default class DappService {
   constructor() {
-    this.web3 = new Web3(Web3.givenProvider || window.web3.currentProvider || window.web3.givenProvider)
+    this.web3 = getWeb3Instance();
     //for older verions of web3
     if (this.web3 && this.web3.net && !this.web3.eth.net){
       this.web3.eth.net = this.web3.net
     }
+  }
+
+  getAccount = (callback) => {
+    this.web3.eth.getAccounts((error, result) => {
+        if (error || result.length === 0) {
+          console.log("Cannot get account");
+        } else {
+            callback(result[0]);
+        }
+    })
+  }
+
+  sendTransaction = (txObject) => {
+    return new Promise((resolve, reject) => {
+      this.web3.currentProvider.sendAsync({
+        method: 'eth_sendTransaction',
+        params: [txObject],
+        from: txObject.from,
+      }, function (data, response) {
+        if (response.error) {
+          reject(response.error.message);
+        }
+
+        resolve(response.result);
+      });
+    });
   }
 } 
