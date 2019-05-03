f6cf603b69c918865f28e530afc30246f5e57e93
Mark Nguyen <ducthuan2807@gmail.com>
2019-05-03 23:47:09 +0700

    update UI on tomowallet (#100)

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -6,23 +6,25 @@ class Footer extends Component {
   render() {
     return (
       <div className={"footer"}>
-        <div className={"footer__container"}>
-          <div className={"container common__flexbox"}>
-            <div className={"footer__content"}>
-              <a href="/" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.ChainTEX")}</a>
-              <a href="#aboutus" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.About_Us")}</a>
-              <a href="/" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.FAQ")}</a>
-              <a href="https://goo.gl/forms/PPgKR2d6A5KtV7tH2" target="_blank" rel="noopener noreferrer" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.Contact_Us")}</a>
-              <a href="https://medium.com/@tomoswap" target="_blank" rel="noopener noreferrer" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.Blog")}</a>
-            </div>
-            <div className={"footer__logo"}>
-              <a href="https://t.me/tomochain" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item telegram"}/></a>
-              <a href="https://twitter.com/SwapTomo" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item twitter"}/></a>
-              <a href="https://www.reddit.com/user/TomoSwap" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item reddit"}/></a>
-              <a href="https://medium.com/@tomoswap" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item medium"}/></a>
+        {!this.props.isTomoWallet && (
+          <div className={"footer__container"}>
+            <div className={"container common__flexbox"}>
+              <div className={"footer__content"}>
+                <a href="/" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.ChainTEX")}</a>
+                <a href="#aboutus" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.About_Us")}</a>
+                <a href="/" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.FAQ")}</a>
+                <a href="https://goo.gl/forms/PPgKR2d6A5KtV7tH2" target="_blank" rel="noopener noreferrer" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.Contact_Us")}</a>
+                <a href="https://medium.com/@tomoswap" target="_blank" rel="noopener noreferrer" className={"footer__content-item"}>{this.props.translate("components.layouts.Footer.Blog")}</a>
+              </div>
+              <div className={"footer__logo"}>
+                <a href="https://t.me/tomochain" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item telegram"}/></a>
+                <a href="https://twitter.com/SwapTomo" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item twitter"}/></a>
+                <a href="https://www.reddit.com/user/TomoSwap" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item reddit"}/></a>
+                <a href="https://medium.com/@tomoswap" target="_blank" rel="noopener noreferrer"><span className={"footer__logo-item medium"}/></a>
+              </div>
             </div>
           </div>
-        </div>
+        )}
 
         <div className={"footer__copyright"}>{this.props.translate("components.layouts.Footer.2019_ChainTEX_All_rights_reserved")}</div>
       </div>
