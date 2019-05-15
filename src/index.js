f756e0f14ad2943f3dc41052d2980bd7c6ebb463
manhlx <manhlx.300695@gmail.com>
2019-05-15 21:10:26 +0700

    Remove unused check

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { LocalizeProvider } from 'react-localize-redux';
-import App from './components/layouts/App';
 import CommingSoon from './components/layouts/CommingSoon';
 import * as serviceWorker from './serviceWorker';
 import { store } from "./store";
-import EnvConfig from "./config/env"
 
 render(
   <LocalizeProvider store={store}>
     <Provider store={store}>
-      {!!EnvConfig.IS_TESTNET ? <App/> : <CommingSoon/>}
-      
+      <CommingSoon/>
     </Provider>
   </LocalizeProvider>,
   document.getElementById('root'),
