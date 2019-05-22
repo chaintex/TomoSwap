6d8f7b1ab253747103f51b9b54fbe574d54d0545
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2019-05-22 16:56:00 +0700

    change file name

--- src/config/env.js
+++ src/config/env.js
@@ -12,7 +12,7 @@ const EnvConfig = {
   TRADE_TOPIC: "0x314089036943f0e5ddddd6939d359902c01dac1be72c517cc8342fca023ad71e",
   METADATA_TRANSFER_DEFINED: "tomochain:trc20?origin=tomoswap&type=transfer&address={0}&recipient={1}&amount={2}",
   METADATA_SWAP_DEFINED: "tomochain:trc20?origin=tomoswap&type=swap&src={0}&srcAmount={1}&dest={2}&minRate={3}&walletId={4}",
-  METAMASK_INSTRUCTION_LINK: "https://testnet.tomoswap.com/metamask_instruction_testnet.pdf",
+  METAMASK_INSTRUCTION_LINK: "/metamask_instruction_testnet.pdf",
 };
 
 export default EnvConfig;
