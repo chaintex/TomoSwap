bdb3099d99b5711bcc92dac834913bc439b369c5
Nguyen Duc Thuan <ducthuan2807@gmail.com>
2019-05-30 13:54:31 +0700

    add countdown

--- src/components/layouts/CommingSoon.js
+++ src/components/layouts/CommingSoon.js
@@ -22,6 +22,10 @@ class CommingSoon extends Component {
 
     this.state = {
       isMobileMenuActive: false,
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
     }
   }
 
@@ -38,6 +42,42 @@ class CommingSoon extends Component {
     return translationId;
   };
 
+  componentDidMount() {
+    this.setCountdown();
+  }
+
+  getCustomDate = (input, zone) => {
+    let d = new Date();
+    let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    let result = new Date(utc + (3600000*(zone)));
+    console.log(result);
+    return result;
+  }
+
+  setCountdown = () => {
+    var countDownDate = new Date("June 4, 2019 17:00:00 GMT+0800").getTime();
+    var x = setInterval(function() {
+      let d = new Date();
+      let utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+      let now = new Date(utc + (3600000*(+8))).getTime();
+    
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+    
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      this.setState({days, hours, minutes, seconds});
+
+      // If the count down is finished, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+      }
+    }, 1000);
+  }
   render() {
     const { code, name } = currentLanguage();
 
@@ -74,7 +114,51 @@ class CommingSoon extends Component {
           <div className={"container"}>
             <div className={"body__container"}>
               <div className={"body__content"}>
-                <h3 className={"body__title"}>{this.props.translate("components.layouts.Body.Comming_Soon")}</h3>
+                <div className={"body__title"}>{this.props.translate("components.layouts.Body.Comming_Soon")}</div>
+                <div className="body__countdown_wrap">
+                  <div className={"body__countdown"}>
+                    <div className="body__countdown_item">
+                      <div className="body__countdown_item_box">
+                        <div className="body__countdown_item_box_number">
+                          02
+                        </div>
+                      </div>
+                      <div className="body__countdown_item_unit">
+                      {this.props.translate("components.layouts.Body.Days")}
+                      </div>
+                    </div>
+                    <div className="body__countdown_item">
+                      <div className="body__countdown_item_box">
+                        <div className="body__countdown_item_box_number">
+                          02
+                        </div>
+                      </div>
+                      <div className="body__countdown_item_unit">
+                      {this.props.translate("components.layouts.Body.Hours")}
+                      </div>
+                    </div>
+                    <div className="body__countdown_item">
+                      <div className="body__countdown_item_box">
+                        <div className="body__countdown_item_box_number">
+                          02
+                        </div>
+                      </div>
+                      <div className="body__countdown_item_unit">
+                      {this.props.translate("components.layouts.Body.Minutes")}
+                      </div>
+                    </div>
+                    <div className="body__countdown_item">
+                      <div className="body__countdown_item_box">
+                        <div className="body__countdown_item_box_number">
+                          02
+                        </div>
+                      </div>
+                      <div className="body__countdown_item_unit">
+                      {this.props.translate("components.layouts.Body.Seconds")}
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <p className={"body__subtitle"}>{this.props.translate("components.layouts.Body.You_can_try_testnet_version_here")} <a href="https://testnet.tomoswap.com" target="_blank" rel="noopener noreferrer">{this.props.translate("components.account.ImportAccountView.here")}</a></p>
               </div>
             </div>
