b6ccd6ca7259454119386d9dc64568d8bfa56b9a
Mark Nguyen <ducthuan2807@gmail.com>
2019-06-06 16:17:19 +0700

    fix the DEDUCT_AMOUNT_FOR_TX_FEE value (#141)

--- src/config/app.js
+++ src/config/app.js
@@ -25,7 +25,8 @@ const AppConfig = {
   WALLET_TYPE_KEYSTORE: 'Keystore',
   WALLET_TYPE_PRIVATE_KEY: 'Private Key',
   MAX_PRECISION: 6,
-  MIN_TRADE: 0.1
+  MIN_TRADE: 0.1,
+  DEDUCT_AMOUNT_FOR_TX_FEE: 0.1
 };
 
 export default AppConfig;
