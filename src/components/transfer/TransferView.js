1a089e39703f1f092587f5afbb34c37aade1c0d6
Mark Nguyen <ducthuan2807@gmail.com>
2019-06-06 10:28:08 +0700

    merge from develop

--- src/components/transfer/TransferView.js
+++ src/components/transfer/TransferView.js
@@ -59,7 +59,7 @@ class TransferView extends Component {
             <div className={`exchange__button common__button-gradient ${disabledClass}`} onClick={() => this.props.openConfirmModal()}>{this.props.translate("components.transfer.TransferView.Transfer_Now")}</div>
             <div className={`exchange-terms ${disableTacClass}`}>
               <span>{this.props.translate("components.swap.SwapView.By_Swapping_you_agree_to_the")}
-              <a className="exchange-terms__link" href={`https://tomoswap.com/tac.pdf`} target="_blank" rel="noopener noreferrer"> {this.props.translate("components.swap.SwapView.Terms_and_Conditions")}</a></span>
+              <a className="exchange-terms__link" href={`/tac.html`} target={aTarget} rel="noopener noreferrer"> {this.props.translate("components.swap.SwapView.Terms_and_Conditions")}</a></span>
             </div>
           </div>
         }
