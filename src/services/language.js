92000bfe896f0008e1c0ad174dbd7e54596e24d7
Mark Nguyen <ducthuan2807@gmail.com>
2019-06-07 15:10:21 +0700

    merge from develop

--- src/services/language.js
+++ src/services/language.js
@@ -8,6 +8,12 @@ function getParameterByName(name, url) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+function getParameterStorage(name) {
+  var result = localStorage.getItem(name);
+  if (!result) return null;
+  return result;
+}
+
 const avalableLanguages = [
     { name: 'English', code: 'en' },
     { name: 'Tiếng Việt', code: 'vi' }
@@ -17,18 +23,21 @@ function currentLanguage(){
   const defaultLanguage = "en";
   let languagePack, packName;
   try {
-    packName = getParameterByName('lang')
+    packName = getParameterByName('lang');
     if (packName === null) {
-      var rawPackName = getParameterByName('locale')
-      switch (rawPackName) {
-        case "en-EN":
-          packName = "en";
-          break;
-        case "vi-VN":
-          packName = "vi";
-          break;
-        default:
-          break;
+      packName = getParameterStorage('lang');
+      if (packName === null) {
+        var rawPackName = getParameterByName('locale')
+        switch (rawPackName) {
+          case "en-EN":
+            packName = "en";
+            break;
+          case "vi-VN":
+            packName = "vi";
+            break;
+          default:
+            break;
+        }
       }
     }
 
@@ -38,6 +47,8 @@ function currentLanguage(){
       packName = defaultLanguage
       languagePack = require("../assets/translations/" + packName + ".json")  
     }
+    
+    localStorage.setItem('lang', packName);
   } catch (e) {
     packName = defaultLanguage
     languagePack = require("../assets/translations/" + packName + ".json")
