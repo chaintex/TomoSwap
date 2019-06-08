0d001fb7dc2601b235f6a56fe1d9517625dd44e9
Mark Nguyen <ducthuan2807@gmail.com>
2019-06-08 15:42:50 +0700

    Add faq to master (#147)
    * update link to FAQ (#131)
    
    * merge from develop
    
    * fix bug responsive on faq page (#145)
    
    * update css (#146)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { LocalizeProvider } from 'react-localize-redux';
 import App from './components/layouts/App';
+import Faq from './components/layouts/FAQ';
 import * as serviceWorker from './serviceWorker';
 import { store } from "./store";
 
 render(
   <LocalizeProvider store={store}>
     <Provider store={store}>
-      <App/>
+      <Router>
+        <Route exact path="/" component={App} />
+        <Route exact path="/faq" component={Faq} />
+      </Router>
     </Provider>
   </LocalizeProvider>,
   document.getElementById('root'),
