4f490b8efa0c3ba90cad5f324d75837f72eedab3
Mark Nguyen <ducthuan2807@gmail.com>
2019-06-11 16:48:52 +0700

    fix bug onpen with target blank on tomowallet (#160)

--- src/components/faq/FaqView.js
+++ src/components/faq/FaqView.js
@@ -8,13 +8,21 @@ class FaqView extends Component {
       return {__html: html};
     }
 
+    const replateTarget = (text, replaceTo = "_self") => {
+      if (text && text.length > 0) {
+        return text.replace(/target=["']_blank["']/g,"target='" + replaceTo + "'");
+      }
+
+      return text;
+    }
+
     const getListFaq = () => {
       return faq[this.props.languageCode].map((item, index) => {
         return (
           <div className="item" key={index}>
             <h3 className="question">{index + 1}{". "}{item.question}</h3>
             <div className="answer">
-              <div dangerouslySetInnerHTML={createMarkup(item.answer)} />
+              <div dangerouslySetInnerHTML={createMarkup(this.props.isTomoWallet ? replateTarget(item.answer) : item.answer)} />
             </div>
           </div>
         )
