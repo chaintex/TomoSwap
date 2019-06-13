8af94d12912d65580020a0b45c3ec7b29ef9ab46
Mark Nguyen <ducthuan2807@gmail.com>
2019-06-13 15:14:13 +0700

    change url when click to swap/transfer button (#167)
    * change url when click to swap/transfer button
    
    * change url when change source token

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import { LocalizeProvider } from 'react-localize-redux';
 import App from './components/layouts/App';
 import Faq from './components/layouts/FAQ';
+import { PAIR_DEFAULT } from './config/tokens';
 import * as serviceWorker from './serviceWorker';
 import { store } from "./store";
 
@@ -12,7 +13,10 @@ render(
   <LocalizeProvider store={store}>
     <Provider store={store}>
       <Router>
-        <Route exact path="/" component={App} />
+        <Route exact path="/" render={() => (
+            <Redirect to={`/swap/${PAIR_DEFAULT.src}-${PAIR_DEFAULT.dest}`} />
+          )}
+        />
         <Route exact path="/:mode(swap|transfer)/:q?" component={App} />
         <Route exact path="/faq" component={Faq} />
       </Router>
